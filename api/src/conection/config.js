b3c6e048841e26fc966b98a7d1ef8c3b057bea8c
Agustin Asef <agustinbattiganeasef@gmail.com>
2023-09-09 18:33:12 -0300

    cambios en la config

--- api/src/conection/config.js
+++ api/src/conection/config.js
@@ -2,6 +2,7 @@ const sqlConfig = {
     host: process.env.DBHost,
     user: process.env.DBUser,
     password: process.env.DBPassword,
+    port: process.env.DBPort,
     database: process.env.DBName
   }
 
